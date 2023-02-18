dcd235f74e0444f79eec3134c3ce85f659f51854
Felix Prima <felix.prima@binus.ac.id>
2023-02-19 02:09:20 +0700

    Home Page Initialization

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,66 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid d-flex" style="font-family: 'Courier New', Courier, monospace; ">
+
+        <a class="navbar-brand" href="#">Jramedia </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
+
+            <div class="nav-right-left me-3">
+                <ul class="navbar-nav mb-2 mb-lg-0">
+
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">View Products</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">View All Transactions</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">View Account</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="nav-right-right d-flex">
+
+                <div class="nav-right-right-left me-4">
+                    <form class="d-flex">
+                        <input class="form-control" type="search" placeholder="Search product here"
+                            aria-label="Search">
+                        <button class="btn btn-outline-success " type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                          </svg></button>
+                    </form>
+                    
+                </div>
+
+                <div class="nav-right-right-right">
+                    <div class="collapse navbar-collapse me-5" id="navbarNavDarkDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown dropdown-menu-lg-end"">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink"
+                                    role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Profile
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-dark "
+                                    aria-labelledby="navbarDarkDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                        
+            </div>
+
+
+        </div>
+</nav>
